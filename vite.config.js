ce3ad8bc75521e7ec551be9c407a6311746f516e
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-22 16:33:54 +0530

    react react-dom package update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; 
 
 export default defineConfig({ 
   plugins: [react()],
+ 
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
   css: {
     lightningcss: {
       errorRecovery: true
-    }
+    },
+    
   }
 });
